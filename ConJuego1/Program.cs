968e99a663a9393620bfb3d9ea38b006bc9edf31
TRP1991 <ta.rubilar@gmail.com>
2021-12-09 10:52:47 -0300

    Se añaden condiciones y reglas

--- ConJuego1/Program.cs
+++ ConJuego1/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Juego de matriz binaria 
+
+/* Condiciones 
+ - Crear una matriz de 4 x 4 de ceros (matriz inicial).
+ - El jugador tendrá 4 turnos para jugar
+ - El jugador deberá indicar las coordenadas X Y del dígito que quiere cambiar de 0 a 1. Dicho dígito cambiará a 1 los ceros 
+ que se encuentren en todas las direcciones posibles (arriba, abajo, izquierda, derecha)
+ - Gana quien logre convertir todos los ceros de la matriz en unos*/
+
 namespace ConJuego1
 {
     class Program
